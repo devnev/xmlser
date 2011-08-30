5d0b9f3cf155cbd13f788ad23d5c7b3fd9ed31c0
Mark Nevill <mark@nevill.ch>
2011-08-30 23:20:25 +0200

    Documented and refactored xmlser, added support for indirection in various places.

--- xmlser.py
+++ xmlser.py
@@ -57,7 +57,7 @@ def force_unicode(txt):
             pass
     raise ValueError("Unable to force %s object %r to unicode" % (type(orig).__name__, orig))
 
-_xml_tag_chr_re = re.compile('[<>&"\']|\\s')
+_xml_tag_badchr_re = re.compile('[<>&"\']|\\s')
 def check_tag(t):
     if not t:
         raise ValueError("XML tag must not be empty")
@@ -65,7 +65,7 @@ def check_tag(t):
         raise ValueError("XML tag must not start with a digit")
     if t[:3].lower() == "xml":
         raise ValueError("XML tag must not start with \"xml\"")
-    if _xml_tag_chr_re.search(t):
+    if _xml_tag_badchr_re.search(t):
         raise ValueError("XML tag must not contain special characters")
     return t
 
@@ -79,6 +79,7 @@ def check_attr(a):
     return a
 
 class _ListStream(object):
+    """Used as "stream" for unicode characters (StringIO et al require bytes)"""
     def __init__(self):
         self.parts = []
         self._val = None
@@ -94,6 +95,7 @@ class _ListStream(object):
         self._parts = []
 
 class StreamWriteEncoder(object):
+    """Wrapper around streams that encodes unicode characters before writing them"""
     def __init__(self, stream, encoding=None):
         self.stream = stream
         self.encoding = encoding
@@ -109,160 +111,147 @@ class StreamWriteEncoder(object):
     def __getattr__(self, attr):
         return getattr(self.stream, attr)
 
+# special object used when skipping through the format string, e.g. because a
+# list was empty.
 _skip = "__skip"
 
 _Element = collections.namedtuple('Element', 'tag attrs content')
 
 class Serializer(object):
 
+    # special characters in format strings
     _special = '*=<>?.&~"'
-    _xml_special = '& \t\n<>"'
 
     def __init__(self, fmt):
         self.fmt = fmt
         super(Serializer, self).__init__()
 
     def _get(self, idx, obj):
+        """After a dot or similar, look up a value"""
+
         if self.fmt[idx].isalpha():
+            # identifier, could be dict key or attribute
             attr = ""
             while self.fmt[idx].isalnum() or self.fmt[idx] == '_':
                 attr += self.fmt[idx]
                 idx += 1
+
+            # if skipping, do not peform lookup
             if obj is _skip:
                 return idx, _skip
+
+            # try to lookup value for attr
             if isinstance(obj, dict):
                 return idx, obj[attr]
             else:
                 return idx, getattr(obj, attr)
+
         elif self.fmt[idx].isdigit():
+            # number, could be dict key or index
             num = ""
             while self.fmt[idx].isdigit():
                 num += self.fmt[idx]
                 idx += 1
+
+            # if skipping, do not peform lookup
             if obj is _skip:
                 return idx, _skip
+
+            # try to lookup value for num
             try:
                 return idx, obj[int(num)]
             except KeyError:
                 return idx, obj[num]
             except IndexError as e:
                 raise KeyError(str(e))
+
         elif self.fmt[idx] == '?':
+            # identity operation
             return idx + 1, obj
+
         else:
             raise InvalidAttribute(self.fmt, idx)
 
     def _val(self, idx, obj):
+        """Get a single value, either from a literal or from a lookup"""
+
         if self.fmt[idx] == '.':
+            # lookup
             while self.fmt[idx] == '.':
                 idx, obj = self._get(idx+1, obj)
             return idx, obj
+
         elif self.fmt[idx] == '"':
+            # quoted string literal
             val = ""
             idx += 1
             end = idx
             while self.fmt[end] != '"':
                 if self.fmt[end] == '\\':
+                    # skip escaped characters
                     end += 1
                 end += 1
             val = self.fmt[idx:end]
+            # decode escapes
             val = val.decode('string_escape')
             return end+1, val
+
+        elif self.fmt[idx].isalpha():
+            # unquoted string literal
+            val = ""
+            while self.fmt[idx] not in self._special and not self.fmt[idx].isspace():
+                val += self.fmt[idx]
+                idx += 1
+            return idx, val
+
         else:
             raise InvalidValue(self.fmt, idx)
 
     def _list(self, idx, obj):
+        """Get a list, e.g. for repetition. Always returns a list so index lookup is possible"""
+
         if self.fmt[idx] == '.':
+            # lookup object
             idx, res = self._get(idx+1, obj)
             if hasattr(res, 'items'):
+                # if its a mapping, get an item list
                 res = res.items()
             return idx, res
+
         elif self.fmt[idx].isdigit():
+            # repeat a fix number of times
             num = ""
             while self.fmt[idx].isdigit():
                 num += self.fmt[idx]
                 idx += 1
             return idx, list(range(int(num)))
+
         else:
             raise InvalidRepetition(self.fmt, idx)
 
-    def _tag_rest(self, idx, tag, cur, obj):
-        try:
-            tag = check_tag(force_unicode(tag))
-        except ValueError as e:
-            raise InvalidName(self.fmt, idx, str(e))
-
-        if self.fmt[idx] == '*' and cur is None:
-            raise InvalidRepetition("Cannot repeat root tag")
-        if self.fmt[idx] == '*':
-            idx += 1
-            idx, l = self._list(idx, obj)
-        else:
-            l = [obj]
-
-        if not l or obj is _skip:
-            # empty list, parse to get ending idx but do not use
-            e = _Element(tag, [], [])
-            idx = self._intag(idx, e, _skip)
-        else:
-            idx_ = idx
-            for item in l:
-                e = _Element(tag, [], [])
-                idx = idx_
-                while self.fmt[idx] != '>':
-                    idx = self._intag(idx, e, item)
-                if cur is not None:
-                    cur.content.append(e)
-
-        if self.fmt[idx] != '>':
-            raise InvalidTag(self.fmt, idx, "Tag not terminated with '>'")
-
-        if cur is None:
-            return idx + 1, e
-        else:
-            return idx + 1
-
-    def _tag(self, idx, cur, obj):
-        if self.fmt[idx] == '<':
-            tag = ""
-            idx += 1
-            if self.fmt[idx] == '~' and cur is None:
-                raise InvalidRepetition("Cannot have multiple root tags")
-            if self.fmt[idx] == '~':
-                idx += 1
-                idx, obj = self._val(idx, obj)
-                if not obj or obj is _skip:
-                    idx = self._tag_rest(idx, ":", cur, _skip)
-                else:
-                    idx_ = idx
-                    for k, v in obj.iteritems():
-                        idx = idx_
-                        idx = self._tag_rest(idx, k, cur, v)
-                    return idx
-            else:
-                while self.fmt[idx] not in self._special:
-                    tag += self.fmt[idx]
-                    idx += 1
-                return self._tag_rest(idx, tag, cur, obj)
-
-        else:
-            raise InvalidTag(self.fmt, idx)
-
     def _attr(self, idx, cur, obj):
-        assert self.fmt[idx] == '='
+        """Handle an attribute. idx must be on the '='."""
+
+        if self.fmt[idx] != '=':
+            raise InvalidAttribute(self.fmt, idx)
         idx += 1
-        attr = ""
-        if not self.fmt[idx].isalpha():
+
+        try:
+            idx, attr = self._val(idx, obj)
+        except InvalidValue:
             raise InvalidName(self.fmt, idx, "Invalid attribute name")
-        while self.fmt[idx] not in self._special:
-            attr += self.fmt[idx]
-            idx += 1
+
         idx, v = self._val(idx, obj)
+
+        # only actually add the attribute if we're not skipping
         if obj is not _skip:
             cur.attrs.append((check_attr(force_unicode(attr)), escape(force_unicode(v))))
+
         return idx
 
     def _intag(self, idx, cur, obj):
+        """Handle children and attributes of tag."""
+
         if self.fmt[idx] == '<':
             return self._tag(idx, cur, obj)
 
@@ -281,6 +270,86 @@ class Serializer(object):
 
         else:
             raise InvalidTag(self.fmt, idx, "Unrecognized character %s" % self.fmt[idx])
+
+    def _tag_rep(self, idx, tag, cur, obj):
+        """Handle possible repetition of tag. `cur is None` implies this is the root tag."""
+
+        try:
+            tag = check_tag(force_unicode(tag))
+        except ValueError as e:
+            raise InvalidName(self.fmt, idx, str(e))
+
+        # handle repetition
+        if self.fmt[idx] == '*' and cur is None:
+            raise InvalidRepetition("Cannot repeat root tag")
+        if self.fmt[idx] == '*':
+            idx += 1
+            idx, l = self._list(idx, obj)
+        else:
+            l = [obj]
+
+        if not l or obj is _skip:
+            # empty list or skipping, parse to get ending idx but do not use
+            e = _Element(tag, [], [])
+            idx = self._intag(idx, e, _skip)
+        else:
+            idx_ = idx
+            for item in l:
+                e = _Element(tag, [], [])
+                idx = idx_
+                while self.fmt[idx] != '>':
+                    idx = self._intag(idx, e, item)
+                if cur is not None:
+                    cur.content.append(e)
+
+        if self.fmt[idx] != '>':
+            raise InvalidTag(self.fmt, idx, "Tag not terminated with '>'")
+
+        if cur is None:
+            # in special case of root, we return the idx and the element
+            return idx + 1, e
+        else:
+            return idx + 1
+
+
+    def _tag_keyrep(self, idx, cur, obj):
+        """
+        Handle dict-based tag generation (key is tag, value is inner object).
+        `cur is None` implies this is the root tag.
+        """
+
+        tag = ""
+
+        # handle dict-based tag generation
+        if self.fmt[idx] == '~' and cur is None:
+            raise InvalidRepetition("Cannot have multiple root tags")
+        if self.fmt[idx] == '~':
+            idx += 1
+            idx, obj = self._val(idx, obj)
+            if not obj or obj is _skip:
+                # empty dict or skipping, parse to get end position but discard
+                # ":" is just an arbitrary valid tag name, it is discarded anyway
+                idx = self._tag_rest(idx, ":", cur, _skip)
+            else:
+                idx_ = idx
+                for k, v in obj.iteritems():
+                    idx = idx_
+                    idx = self._tag_rep(idx, k, cur, v)
+                return idx
+        else:
+            # extract tag name
+            idx, tag = self._val(idx, obj)
+            return self._tag_rep(idx, tag, cur, obj)
+
+    def _tag(self, idx, cur, obj):
+        """Handle a tag. `cur is None` implies this is the root tag."""
+
+        if self.fmt[idx] != '<':
+            raise InvalidTag(self.fmt, idx)
+
+        idx += 1
+
+        return self._tag_keyrep(idx, cur, obj)
 
     def _write(self, stream, tree):
 
---
+++ xmlser_tests.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+# Copyright 2011 Mark Nevill
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import xmlser
+import unittest
+
+class SerializationTests(unittest.TestCase):
+
+    def cmp_none(self, ser, exp_fmt):
+        self.assertEqual(ser.serialize(None), exp_fmt)
+        self.assertEqual(ser.serialize([]), exp_fmt)
+        self.assertEqual(ser.serialize(1), exp_fmt)
+        self.assertEqual(ser.serialize([1]), exp_fmt)
+
+    def cmp_obj(self, ser, exp_fmt):
+        self.assertEqual(ser.serialize(None), exp_fmt % None)
+        self.assertEqual(ser.serialize(1), exp_fmt % 1)
+        self.assertEqual(ser.serialize("a"), exp_fmt % "a")
+        self.assertEqual(ser.serialize(u"ä"), exp_fmt % u"ä")
+
+    def cmp_dictitem(self, ser, exp_fmt):
+        self.assertEqual(ser.serialize({'item':None}), exp_fmt % None)
+        self.assertEqual(ser.serialize({'item':1}), exp_fmt % 1)
+        self.assertEqual(ser.serialize({'item':"a"}), exp_fmt % "a")
+        self.assertEqual(ser.serialize({'item':u"ä"}), exp_fmt % u"ä")
+
+    def cmp_listitem(self, ser, exp_fmt):
+        ser_ = ser
+        for i in xrange(0, 5):
+            ser = xmlser.Serializer(ser_ % dict(idx=i))
+            for o in (None, 1, "a", u"ä"):
+                l = [None]*i + [o]
+                self.assertEqual(ser.serialize(l), exp_fmt % dict(obj=o))
+
+    def test_root(self):
+        ser = xmlser.Serializer("<root>")
+        exp = "<root></root>"
+        self.cmp_none(ser, exp)
+
+    def test_sub(self):
+        ser = xmlser.Serializer("<root<sub>>")
+        exp = "<root><sub></sub></root>"
+        self.cmp_none(ser, exp)
+
+    def test_3sub(self):
+        ser = xmlser.Serializer("<root<sub*3>>")
+        exp = "<root><sub></sub><sub></sub><sub></sub></root>"
+        self.cmp_none(ser, exp)
+
+    def test_attr_lit(self):
+        ser = xmlser.Serializer("<root<sub=attr\"test\">>")
+        exp = "<root><sub attr=\"test\"></sub></root>"
+        self.cmp_none(ser, exp)
+
+    def test_attr_obj(self):
+        ser = xmlser.Serializer("<root<sub=attr.?>>")
+        exp = u"<root><sub attr=\"%s\"></sub></root>"
+        self.cmp_obj(ser, exp)
+
+    def test_attr_dictitem(self):
+        ser = xmlser.Serializer("<root<sub=attr.item>>")
+        exp = u"<root><sub attr=\"%s\"></sub></root>"
+        self.cmp_dictitem(ser, exp)
+
+    def test_attr_listitem(self):
+        ser = "<root<sub=attr.%(idx)s>>"
+        exp = u"<root><sub attr=\"%(obj)s\"></sub></root>"
+        self.cmp_listitem(ser, exp)
+
+    def test_content_lit(self):
+        ser = xmlser.Serializer("<root&\"test\">")
+        exp = u"<root>test</root>"
+        self.cmp_none(ser, exp)
+
+    def test_content_obj(self):
+        ser = xmlser.Serializer("<root&.?>")
+        exp = u"<root>%s</root>"
+        self.cmp_obj(ser, exp)
+
+    def test_content_dictitem(self):
+        ser = xmlser.Serializer("<root&.item>")
+        exp = u"<root>%s</root>"
+        self.cmp_dictitem(ser, exp)
+
+    def test_content_listitem(self):
+        ser = "<root&.%(idx)s>"
+        exp = u"<root>%(obj)s</root>"
+        self.cmp_listitem(ser, exp)
+
+    def test_rep_list(self):
+        ser = xmlser.Serializer("<root<sub*.?&.?>>")
+        self.assertEqual(ser.serialize(['a', 'b']), "<root><sub>a</sub><sub>b</sub></root>")
+
+    def test_rep_dict(self):
+        ser = xmlser.Serializer("<root<sub*.?=k.0=v.1>>")
+        self.assertEqual(ser.serialize({'a':1, 'b':2}),
+                         '<root><sub k="a" v="1"></sub><sub k="b" v="2"></sub></root>')
+
+    def test_sub_dicttag(self):
+        ser = xmlser.Serializer("<root<~.?&.?>>")
+        self.assertEqual(ser.serialize({'a':1}), "<root><a>1</a></root>")
+
+if __name__ == "__main__":
+    unittest.main()
